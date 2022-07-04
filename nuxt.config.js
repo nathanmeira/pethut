da9a03dad19878581b5fea5acadde16c7bceab6d
nathanmeira <nathan.meira1@gmail.com>
2022-07-03 21:13:32 -0300

    gh pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/pethut/'
+  },
   head: {
     title: 'pet-hut',
     htmlAttrs: {
