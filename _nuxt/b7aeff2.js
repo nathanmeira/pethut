7232fdef3af24eb41b494cf283abaae3aca97721
nathanmeira <nathan.meira1@gmail.com>
2022-07-03 21:44:31 -0300

    d527e03

---
+++ _nuxt/b7aeff2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{178:function(n,o,w){w(179),n.exports=w(180)}},[[178,22,16,23]]]);
---
+++ _nuxt/static/1656895442/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpethut\u002F_nuxt\u002Fstatic\u002F1656895442",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpethut\u002F",assetsPath:"\u002Fpethut\u002F_nuxt\u002F",cdnURL:a}}}}(null));
